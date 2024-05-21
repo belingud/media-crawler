c15e0887d1f5e5879b80ed6e33fd7bd27c69e0c2
belingud <1170202353@qq.com>
2024-05-21 23:16:46 +0800

    update pm2 runtime node arg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: 'dist/src/main.js',
       watch: false,
       namespace: 'crawler',
+      node_args: ['-r ./.pnp.cjs'],
       autorestart: true,
       max_restarts: 5, // 最大异常重启次数
       restart_delay: 10, // 异常重启情况下，延时重启时间
